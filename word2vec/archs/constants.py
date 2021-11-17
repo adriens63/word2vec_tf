04e3b2d60aecd785363734e8e4bb27a623cab4b4
adriens63 <serviere.adrienn@gmail.com>
2021-11-17 10:37:58 +0100

    premier commit, constantes définies temporairement

---
+++ word2vec/archs/constants.py
@@ -0,0 +1,11 @@
+CBOW_N_WORDS = 3
+SKIPGRAM_N_WORDS = 3 # contexte = 3 mots avant, 3 mots après le mot target
+
+MIN_WORD_FREQUENCY = 30
+MAX_SEQ_LENGTH = 256 # à definir à zéro si on ne veut pas de troncature des paragraphes
+
+EMBEDDING_DIM = 256
+EMBEDDING_MAX_NORM = 1
+
+VOCAB_SIZE = 4096
+BATCH_SIZE = 32
