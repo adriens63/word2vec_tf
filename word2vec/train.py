a91ef88399b043095b645bf16d1dee981a7aea6e
adriens63 <serviere.adrienn@gmail.com>
2021-12-23 21:20:46 +0000

    modify train

--- word2vec/train.py
+++ word2vec/train.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
+import yaml
+
 import argparse
 
 import word2vec.utils.trainer as t
@@ -11,8 +13,11 @@ import word2vec.archs.constants as c
 
 
 
+# ********************* launch training ***********************
+# cmd to launch : python -m word2vec.train --config '.word2vec/config/config.yaml'
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description = 'word2vec training')
     parser.add_argument('--config', type=str, required=True, help='path to yaml config')
     args = parser.parse_args()
     
